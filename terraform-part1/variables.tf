67b0a1d69240ed519dbc0ff75dab9c5dd87da0a3
raj-kumar-wezo <raj@wezo.co>
2025-09-13 22:20:15 +0530

    ...

--- terraform-part1/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {
-  default = "ap-south-1"
-}
-variable "project_name" { default = "wezo-docker-app" }
-variable "instance_type" { default = "t3.micro" }
-variable "key_name" { default = "wezo-key" }
-variable "public_key_path" { default = "~/.ssh/wezo-key.pem.pub" } # adjust
-variable "repo_url" { description = "Git repo URL containing frontend & backend" }
